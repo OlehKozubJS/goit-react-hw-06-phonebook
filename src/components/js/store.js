4df3caed4f7d8a2b867f9d9a32e4efe5ce4ae111
OlehKozubJS <oleg.kozub54@gmail.com>
2023-08-17 21:26:27 +0300

    Update store.js

--- src/components/js/store.js
+++ src/components/js/store.js
@@ -3,16 +3,8 @@ import { createStore } from 'redux';
 // Початкове значення стану Redux для кореневого редюсера,
 // якщо не передати параметр preloadedState.
 const initialState = {
-  tasks: [
-    { id: 0, text: 'Learn HTML and CSS', completed: true },
-    { id: 1, text: 'Get good at JavaScript', completed: true },
-    { id: 2, text: 'Master React', completed: false },
-    { id: 3, text: 'Discover Redux', completed: false },
-    { id: 4, text: 'Build amazing apps', completed: false },
-  ],
-  filters: {
-    status: 'all',
-  },
+  contacts: [],
+  filter: '',
 };
 
 // Поки що використовуємо редюсер який
