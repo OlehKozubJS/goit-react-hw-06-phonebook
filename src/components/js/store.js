d171d0b73906853eeaaa0dabbac74bb39d01dd56
OlehKozubJS <oleg.kozub54@gmail.com>
2023-08-18 22:04:32 +0300

    Update store.js

--- src/components/js/store.js
+++ src/components/js/store.js
@@ -1,14 +1,10 @@
 import { createStore } from 'redux';
 
-// Початкове значення стану Redux для кореневого редюсера,
-// якщо не передати параметр preloadedState.
 const initialState = {
   contacts: [],
   filter: '',
 };
 
-// Поки що використовуємо редюсер який
-// тільки повертає отриманий стан
 const rootReducer = (state = initialState, action) => {
   return state;
 };
