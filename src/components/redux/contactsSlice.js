bde5017d02d25d4f3155d2eb9c619a3607ad3972
OlehKozubJS <oleg.kozub54@gmail.com>
2023-08-19 21:40:35 +0300

    npm i nanoid, add reduce actions

--- src/components/redux/contactsSlice.js
+++ src/components/redux/contactsSlice.js
@@ -1,11 +1,23 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { nanoid } from 'nanoid';
 
 const contactsInitialState = [];
 
 const contactsSlice = createSlice({
   name: 'contacts',
   initialState: contactsInitialState,
-  reducers: {},
+  reducers: {
+    addNewContact(state, action) {
+      if (
+        state.some(
+          contact => contact.name.toLowerCase() === data.name.toLowerCase()
+        )
+      ) {
+        return;
+      }
+      setContacts(prevState => [...prevState, { id: nanoid(), ...data }]);
+    },
+  },
 });
 
 export const { setContacts } = contactsSlice.actions;
