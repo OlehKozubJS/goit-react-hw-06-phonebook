29aa773692c18ac3d052339459a627f4194f2c79
OlehKozubJS <oleg.kozub54@gmail.com>
2023-08-19 15:13:07 +0300

    Create contactsSlice.js

---
+++ src/components/redux/contactsSlice.js
@@ -0,0 +1,12 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const contactsInitialState = [];
+const filterInitialState = '';
+
+export const contactsReducer = (state = contactsInitialState, action) => {
+  return state;
+};
+
+export const filterReducer = (state = filterInitialState, action) => {
+  return state;
+};
