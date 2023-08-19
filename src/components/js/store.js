30469cdc440bb5e59198f12680aae5591d2997e2
OlehKozubJS <oleg.kozub54@gmail.com>
2023-08-19 15:09:00 +0300

    Update store.js

--- src/components/js/store.js
+++ src/components/js/store.js
@@ -1,20 +1,4 @@
-/*
-const initialState = {
-  contacts: [],
-  filter: '',
-};
-*/
 import { configureStore } from '@reduxjs/toolkit';
-const contactsInitialState = [];
-const filterInitialState = '';
-
-export const contactsReducer = (state = contactsInitialState, action) => {
-  return state;
-};
-
-export const filterReducer = (state = filterInitialState, action) => {
-  return state;
-};
 
 export const store = configureStore({
   reducer: {
