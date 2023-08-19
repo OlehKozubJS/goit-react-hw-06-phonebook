d3307e215e98ea2e18ebfe76da21f70c429266d7
OlehKozubJS <oleg.kozub54@gmail.com>
2023-08-19 16:03:26 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,53 @@
-import { Phonebook } from './Phonebook';
+import { ContactForm } from './ContactForm';
+import { ContactList } from './ContactList';
+import { Filter } from './Filter';
+import { Alert } from './Alert';
+import { nanoid } from 'nanoid';
+import PhonebookStyles from './PhonebookCSS/Pnonebook.module.css';
+import { useState, useEffect } from 'react';
 
 export const App = () => {
+  const [contacts, setContacts] = useState(
+    JSON.parse(localStorage.getItem('contacts')) || []
+  );
+  const [filter, setFilter] = useState('');
+  const [name, setName] = useState('');
+
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
+
+  const addNewContact = data => {
+    if (
+      contacts.some(
+        contact => contact.name.toLowerCase() === data.name.toLowerCase()
+      )
+    ) {
+      setName(data.name);
+      return;
+    }
+    setContacts(prevState => [...prevState, { id: nanoid(), ...data }]);
+    setName('');
+  };
+
+  const filteredContacts = () => {
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filter.toLowerCase())
+    );
+  };
+
+  const closeAlert = () => {
+    setName('');
+  };
+
+  const searchQueryInput = value => {
+    setFilter(value);
+  };
+
+  const deleteContact = id => {
+    setContacts(prevState => prevState.filter(contact => contact.id !== id));
+  };
+
   return (
     <div
       style={{
@@ -10,8 +57,18 @@ export const App = () => {
         fontSize: 40,
         color: '#010101',
       }}
+      className={PhonebookStyles.phonebook}
     >
-      <Phonebook />
+      <h1 className={PhonebookStyles.phonebookHeader}>Phonebook</h1>
+      <ContactForm submitFunction={addNewContact} />
+      <Alert isInContacts={!!name} name={name} clickFunction={closeAlert} />
+      <h2 className={PhonebookStyles.contactsHeader}>Contacts</h2>
+      <Filter className="filterInput" changeFunction={searchQueryInput} />
+      <ContactList
+        className="contactList"
+        items={filter ? filteredContacts() : contacts}
+        clickFunction={deleteContact}
+      />
     </div>
   );
 };
