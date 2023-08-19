e3ee38b50b3997b751304fd31efcd99d4550701f
OlehKozubJS <oleg.kozub54@gmail.com>
2023-08-19 15:46:00 +0300

    Create filterSlice.js

---
+++ src/components/redux/filterSlice.js
@@ -0,0 +1,9 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const filterInitialState = '';
+
+export const filterSlice = createSlice({
+  name: 'filter',
+  initialState: filterInitialState,
+  reducers: {},
+});
