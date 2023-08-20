2f5ffb234d98f4b7e9be34c76d3a9006eb3843d9
OlehKozubJS <oleg.kozub54@gmail.com>
2023-08-20 23:27:28 +0300

    Update filterSlice.js

--- src/components/redux/filterSlice.js
+++ src/components/redux/filterSlice.js
@@ -1,6 +1,6 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-const filterInitialState = '';
+const filterInitialState = { filter: '', filteredContacts: [] };
 
 const filterSlice = createSlice({
   name: 'filter',
