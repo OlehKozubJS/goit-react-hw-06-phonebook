155baf25e9bb91e79146f1cb229a1c59081c80dd
OlehKozubJS <oleg.kozub54@gmail.com>
2023-08-20 20:55:10 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -16,7 +16,6 @@ export const App = () => {
   const contacts = useSelector(state => state.contacts);
   const filter = useSelector(state => state.filter);
   const [name, setName] = useState('');
-  const [renderNumber, setRenderNumber] = useState(0);
   const dispatch = useDispatch();
 
   const filteredContacts = data => {
@@ -36,7 +35,6 @@ export const App = () => {
   };
   return (
     <div className={PhonebookStyles.phonebook}>
-      {setRenderNumber(prevState => (prevState += 1))}
       <h1 className={PhonebookStyles.phonebookHeader}>Phonebook</h1>
       <ContactForm submitFunction={handleAdd} />
       <Alert isInContacts={!!name} name={name} clickFunction={closeAlert} />
