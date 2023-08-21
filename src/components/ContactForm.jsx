7d1b134548f12b720571148be34912acf3da2bdc
OlehKozubJS <oleg.kozub54@gmail.com>
2023-08-21 20:36:59 +0300

    Update ContactForm.jsx

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -26,38 +26,41 @@ export const ContactForm = ({ submitFunction }) => {
   };
 
   return (
-    <form className={ContactFormStyles.contactForm} onSubmit={handleSubmit}>
-      <label htmlFor="name">
-        <h3 className={ContactFormStyles.contactFormInputHeader}>Name</h3>
-        <input
-          type="text"
-          className={ContactFormStyles.contactFormInput}
-          id="name"
-          name="name"
-          pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-          required
-        />
-      </label>
-      <label htmlFor="number">
-        <h3 className={ContactFormStyles.contactFormInputHeader}>Number</h3>
-        <input
-          type="tel"
-          id="number"
-          name="number"
-          className={ContactFormStyles.contactFormInput}
-          pattern="\+?\d{1,4}?[ .\-\s]?\(?\d{1,3}?\)?[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,9}"
-          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-          required
-        />
-      </label>
-      <button
-        className={ContactFormStyles.contactFormSubmitButton}
-        type="submit"
-      >
-        Add contact
-      </button>
-    </form>
+    <>
+      <form className={ContactFormStyles.contactForm} onSubmit={handleSubmit}>
+        <label htmlFor="name">
+          <h3 className={ContactFormStyles.contactFormInputHeader}>Name</h3>
+          <input
+            type="text"
+            className={ContactFormStyles.contactFormInput}
+            id="name"
+            name="name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+          />
+        </label>
+        <label htmlFor="number">
+          <h3 className={ContactFormStyles.contactFormInputHeader}>Number</h3>
+          <input
+            type="tel"
+            id="number"
+            name="number"
+            className={ContactFormStyles.contactFormInput}
+            pattern="\+?\d{1,4}?[ .\-\s]?\(?\d{1,3}?\)?[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            required
+          />
+        </label>
+        <button
+          className={ContactFormStyles.contactFormSubmitButton}
+          type="submit"
+        >
+          Add contact
+        </button>
+      </form>
+      <Alert isInContacts={!!name} name={name} clickFunction={closeAlert} />
+    </>
   );
 };
 
