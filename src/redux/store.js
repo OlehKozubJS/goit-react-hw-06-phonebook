059440f9d5a8c3a5148f27baff5894a4af2e7f26
OlehKozubJS <oleg.kozub54@gmail.com>
2023-08-23 13:07:04 +0300

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -16,7 +16,7 @@ const persistConfig = {
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const store = configureStore({
+export const store = configureStore({
   reducer: persistedReducer,
 });
 
