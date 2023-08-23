c72e201defc31780368b143df45e2df7a24c760b
OlehKozubJS <oleg.kozub54@gmail.com>
2023-08-23 12:32:44 +0300

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { contactsReducer } from './contactsSlice';
 import { filterReducer } from './filterSlice';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 export const store = configureStore({
   reducer: {
